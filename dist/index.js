7ef6f79f3a72fa20789f9533e06ce79a0a6c8ad3
nicholas kipkoech <nickey968@gmail.com>
2023-03-23 12:17:33 +0300

    apollo-server init

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+const typeDefs = `#graphql
+ 
+ type Book{
+    title:String,
+    author:String
+ }
+
+ type Query{
+    books:[Book]
+ }
+ 
+ 
+ `;
+const books = [
+    {
+        title: "the awekening",
+        author: "kate chopin",
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+console.log(`server started at :${url}`);
